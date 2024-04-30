7cebd6b921f1106f488d53b80fb930906a24135b
kukiFelt <108556456+kukiFelt@users.noreply.github.com>
2024-04-30 02:12:46 +0000

    fix: Edição Banner

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -1,7 +1,7 @@
 export default function Banner(){
     return(
         <div className="mt-3 p-8 text-white bg-orange-500">
-           <h2 className="">Let's go now</h2>
+           <h2 className="">Lets go now</h2>
            <h3 className="">explore</h3>
            <h4 className="font-bold">and travel</h4>
            <button className="bg-green-700 rounded-md border-lime-500 text-xs text-center p-2 mt-3">
