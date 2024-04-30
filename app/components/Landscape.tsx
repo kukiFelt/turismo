7fde68e400d3b3ff2e1fe7fc1c4ecb66b4e4c8eb
kukiFelt <108556456+kukiFelt@users.noreply.github.com>
2024-04-30 03:18:49 +0000

    fix: config. Landscape

---
+++ app/components/Landscape.tsx
@@ -0,0 +1,33 @@
+
+
+export default function Landscape(){
+    return(
+        <> <div className="mt-6 gap-2 grid grid-cols-2 justify-items-center">  
+                <div className="">
+                 <a href="">
+                 <img src="/img/1.png" alt="" />
+                 </a>
+                </div>
+
+                <div>
+                    <a href="">
+                    <img src="/img/2.png" alt="" />
+                    </a>
+                </div>
+
+                <div className="">
+                    <a href="">
+                    <img src="/img/3.png" alt="" />
+                    </a>
+                </div>
+
+                <div className="">
+                    <a href="">
+                    <img src="/img/4.png" alt="" />
+                    </a>
+                </div>
+            </div>
+        </>
+    )
+
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Banner from "./components/Banner";
 import Header from "./components/Header";
+import Landscape from "./components/Landscape";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Banner/>
+      <Landscape/>
     </div>
  
   )
