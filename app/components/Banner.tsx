3f2dbd82313e162968b0d027e13728172005ca11
kukiFelt <108556456+kukiFelt@users.noreply.github.com>
2024-04-30 02:02:29 +0000

    fix: Config Banner

---
+++ app/components/Banner.tsx
@@ -0,0 +1,13 @@
+export default function Banner(){
+    return(
+        <div className="mt-3 p-8 text-white bg-orange-500">
+           <h2 className="">Let's go now</h2>
+           <h3 className="">explore</h3>
+           <h4 className="font-bold">and travel</h4>
+           <button className="bg-green-700 rounded-md border-lime-500 text-xs text-center p-2 mt-3">
+            VIEW OUR TOURS
+           </button>
+        </div>
+    )
+    // rounded-md BOTÃO REDONDO
+}
